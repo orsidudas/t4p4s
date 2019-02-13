0b2bdcf4953d8bfff763771401eecc4df4ea6f5b
orsidudas <orsi1293@gmail.com>
2019-02-13 12:31:31 +0100

    update

--- examples/test/test-basic.c
+++ examples/test/test-basic.c
@@ -5,14 +5,11 @@
 
 fake_cmd_t t4p4s_testcase_test[][RTE_MAX_LCORE] = {
     {
-        FSLEEP(200),
         {FAKE_PKT, 0, 1, ETH(ETH01, ETH1A), 200, 11, ETH(ETH01, ETH1A)},
-        {FAKE_PKT, 0, 1, ETH(ETH02, ETH1A), 200, 22, ETH(ETH02, ETH1A)},
-        {FAKE_PKT, 0, 1, ETH(ETH03, ETH1A), 200, 33, ETH(ETH03, ETH1A)},
-        {FAKE_PKT, 0, 1, ETH(ETH04, ETH1A), 200, 44, ETH(ETH04, ETH1A)},
         FEND,
     },
     {
+        {FAKE_PKT, 0, 1, ETH(ETH02, ETH1A), 200, 22, ETH(ETH02, ETH1A)},
         FEND,
     },
 };
